4d4894670e842982dca4f88e33e80a8bd527cb40
peter rader <raderp841@gmail.com>
2018-06-20 16:38:21 -0400

    assembly update

---
+++ ApplesOrdering/ApplesOrdering/Controllers/BakeryUpdateOrderController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using ApplesOrdering.Models;
+using ApplesOrdering.DAL;
+
+namespace ApplesOrdering.Controllers
+{
+    public class BakeryUpdateOrderController : ApiController
+    {
+        [HttpPost]
+        [Route("api/bakeryUpdateOrder/{id}")]
+        public IHttpActionResult UpdateBakeryItem(int id)
+        {
+            OrdersDAL dal = new OrdersDAL();
+            dal.ChangeActivityBakery(id);
+            return Ok();
+        }
+    }
+}
--- DB/data.sql
+++ DB/data.sql
@@ -3,5 +3,10 @@ select * from userInfo;
 insert into userInfo values ('Peter', 'Rader', 'admin@admin.com', 'admin', null, 1, 0, 0);
 insert into userInfo values ('Deli', 'Deli', 'deli@deli.com', 'deli', null, 0, 1, 0);
 insert into userInfo values ('Bakery', 'Bakery', 'bakery@bakery.com', 'bakery', null, 0, 0, 1);
+insert into deliOrder values ('Mr. Frankfort', '555-555-5555', GETDATE(), '5-15-2018 18:44:32', 1, 40, 0, 1);
 
 delete from userInfo where id = 3;
+
+select * from store
+
+insert into store values('Wellington Apples');
